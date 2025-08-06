f8104634308790f8d68bfaaa16ce266dc6f81e6f
ross96D <abelaross96@gmail.com>
2025-08-06 11:39:13 -0400

    aa

--- Makefile
+++ Makefile
@@ -1,18 +1,26 @@
 VERSION = HEAD
+PWD = $$(pwd)
+
+.PHONY: clean build
 
 main.so: main.cpp
 	c++ -std=c++23 --no-gnu-unique -Wall -g  $$(pkg-config --cflags pixman-1 libdrm hyprland) -o main.so main.cpp --shared -fPIC
 
-build: main.so
+hash: main.so
+	shasum main.so | awk '{print $$1}' > hash
+
+
+build: hash
+	mkdir $(PWD)/build/$$(cat hash) 2>/dev/null; cp main.so $(PWD)/build/$$(cat hash)/main.so
 
 clean:
 	rm -f main.so
 
 load:
-	hyprctl plugin load /home/ross/programming/tmp/MyPlugin/main.so
+	hyprctl plugin load $(PWD)/build/$$(cat hash)/main.so
 
 unload:
-	hyprctl plugin unload /home/ross/programming/tmp/MyPlugin/main.so
+	hyprctl plugin unload $(PWD)/build/$$(cat hash)/main.so
 
 list:
 	hyprctl plugin list
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <hyprland/src/plugins/PluginAPI.hpp>
-// #include <hyprland/src/includes.hpp>
+#include <hyprland/src/devices/IKeyboard.hpp>
 #include <hyprland/src/Compositor.hpp>
 
-inline HANDLE  PHANDLE = nullptr;
-FILE*          o_file  = nullptr;
+inline HANDLE        PHANDLE = nullptr;
+FILE*                o_file  = nullptr;
+SP<HOOK_CALLBACK_FN> mouseButtonCb;
+SP<HOOK_CALLBACK_FN> touchDownCb;
+SP<HOOK_CALLBACK_FN> keyPressCb;
 
 APICALL EXPORT std::string PLUGIN_API_VERSION() {
     return HYPRLAND_API_VERSION;
@@ -17,42 +20,44 @@ static void onMouseButton(void* self, std::any data) {
     if (mouseButtonEvent.state != WL_POINTER_BUTTON_STATE_PRESSED) {
         return;
     }
-    if (o_file) {
-        std::string text("onMouseButton\n");
-        fwrite(text.c_str(), 1, text.size(), o_file);
-    }
+    std::string text("onMouseButton\n");
+    fwrite(text.c_str(), 1, text.size(), o_file);
+    fflush(o_file);
 }
 
 static void onTouchDown(void* self, std::any data) {
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
     // data is guaranteed
-    const auto touchDownEvent = std::any_cast<Aquamarine::ITouch::SDownEvent>(data);
+    const auto  touchDownEvent = std::any_cast<Aquamarine::ITouch::SDownEvent>(data);
 
-    if (o_file) {
-        std::string text("onTouchDown\n");
-        fwrite(text.c_str(), 1, text.size(), o_file);
-    }
+    std::string text("onTouchDown\n");
+    fwrite(text.c_str(), 1, text.size(), o_file);
+    fflush(o_file);
 }
 
 static void onKeyPress(void* self, std::any data) {
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
     // data is guaranteed
-    const auto keyPressEvent = std::any_cast<std::unordered_map<std::string, std::any>>(data);
-
-    for (const std::pair<const std::string, std::any>& keyPress : keyPressEvent) {}
+    auto dataMap = std::any_cast<std::unordered_map<std::string, std::any>>(data);
+    auto event   = std::any_cast<IKeyboard::SKeyEvent>(dataMap["event"]);
+    if (event.state != WL_KEYBOARD_KEY_STATE_PRESSED) {
+        return;
+    }
+    std::string text = "onKeyPress " + std::to_string(event.keycode) + "\n";
+    fwrite(text.c_str(), 1, text.size(), o_file);
+    fflush(o_file);
 }
 
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
-    throw std::runtime_error("[MyPlugin] YOLO");
-
     o_file = fopen("/home/ross/programming/tmp/MyPlugin/log.txt", "w+");
     if (o_file == nullptr) {
         HyprlandAPI::addNotification(PHANDLE, "[MyPlugin] COULD NOT OPEN FILE.", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
         throw std::runtime_error("[MyPlugin] Internal state error");
     }
     fprintf(o_file, "%s", "PLUGIN INIT\n");
+    fflush(o_file);
 
     PHANDLE = handle;
 
@@ -65,17 +70,28 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
         throw std::runtime_error("[MyPlugin] Version mismatch");
     }
 
-    static auto p1 = HyprlandAPI::registerCallbackDynamic(PHANDLE, "mouseButton", [&](void* self, SCallbackInfo& info, std::any data) {
+    mouseButtonCb = HyprlandAPI::registerCallbackDynamic(PHANDLE, "mouseButton", [&](void* self, SCallbackInfo& info, std::any data) {
         onMouseButton(self, data);
         return;
     });
 
-    static auto p2 = HyprlandAPI::registerCallbackDynamic(PHANDLE, "touchDown", [&](void* self, SCallbackInfo& info, std::any data) {
+    touchDownCb = HyprlandAPI::registerCallbackDynamic(PHANDLE, "touchDown", [&](void* self, SCallbackInfo& info, std::any data) {
         onTouchDown(self, data);
         return;
     });
 
-    HyprlandAPI::addNotification(PHANDLE, "[MyPlugin] ZZZZInitialized successfully!", CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
+    keyPressCb = HyprlandAPI::registerCallbackDynamic(PHANDLE, "keyPress", [&](void* self, SCallbackInfo& info, std::any data) {
+        onKeyPress(self, data);
+        return;
+    });
+
+    HyprlandAPI::addNotification(PHANDLE, "[MyPlugin] Initialized successfully!", CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
 
     return {"MyPlugin", "Not that amazing plugin to test the world!", "Me", "0.1"};
+}
+
+APICALL EXPORT void PLUGIN_EXIT() {
+    mouseButtonCb.reset();
+    touchDownCb.reset();
+    keyPressCb.reset();
 }
